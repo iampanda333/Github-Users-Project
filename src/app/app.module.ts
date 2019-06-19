b396788ff5b91c02d8687e0b407cdc04888120dd
iampanda333 <dibyajyoti.panda231@gmail.com>
2019-06-19 20:11:52 +0530

    Structure changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule
